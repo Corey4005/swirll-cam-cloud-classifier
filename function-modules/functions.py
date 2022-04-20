39a5f5ed53126d546e1e2103754073faa3cbcab5
Corey4005 <cdw0063@uah.edu>
2022-04-20 11:54:21 -0500

    completed cloud cover function and added data.

--- function-modules/functions.py
+++ function-modules/functions.py
@@ -26,6 +26,10 @@ import shutil
 import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import matplotlib.image as mplim
+import numpy as np
+from datetime import datetime
 
 #the filepath to the swirll images:
 swirlldata = '../Tensorflow/workspace/swirll_demo/images'
@@ -214,8 +218,112 @@ def run_xml_to_csv():
         print('Successfully converted xml to csv.')
     
 
+def cloud_fraction_all_imgs():
+    '''
+    Purpose
+    -------
     
+    This script reads each image in the 2020 dataset and returns a dataframe 
+    with cloud fraction, filepath, and octa score by date. 
+
+    Returns
+    -------
+    df : Pandas DataFrame
     
+        Returns the pandas dataframe for cloud fraction and okta score for each 
+        image in the 2020 dataset.
+    
+    .csv : Excel Workbook
+        
+        Saves a .csv of the Pandas DataFrame in ../swirll-data/ for easy access
+        
+    
+
+    '''
+    
+    dates = []
+    file_names = []
+    #read all the images and get their name
+    for root, directory, file in os.walk(swirlldata):
+        for f in file:
+            if f.endswith('.jpg'):
+        
+        #get file name
+                filepath = os.path.join(swirlldata, f)
+                raw_name = filepath.split('/')[-1]
+                date_string = raw_name[-20:]
+                img_date = datetime.strptime(date_string, "%Y_%m%d_%H%M%S.jpg")
+                dates.append(img_date)
+                file_names.append(filepath)
+    #make the dataframe
+    dic = {'Date' : dates, 'File Names' : file_names}
+    df = pd.DataFrame(data=dic)
+    df.set_index('Date', inplace=True)
+    df.sort_index(inplace=True)
+    
+    #go through the images and read them for cloud fraction
+    cloud_fraction = []
+    cf_oktas = []
+    
+    count = 0
+    for f in df['File Names']:
+        count = count + 1
+        
+        #read the image
+        img_orig = mplim.imread(f)
+        
+        #limit to sky
+        img_orig = img_orig[:,0:1300,:]
+        img = np.int16(img_orig)  # Convert image to 16-bit signed int for computation
+        cf_img = img[:,:,2] - img[:,:,0]  # Get differece between blue and red
+        cf_img = np.where(cf_img < 0, -cf_img, cf_img)  # Absolute value of difference
+        cf_img = np.uint8(cf_img)  # Convert back to 8-bit unsigned integer
+
+        #Get number of pixels above threshold percentage
+        threshold_percentage = 0.10  # approx 20% experimentally observed
+        # threshold_img = (cf_img / 255) > threshold_percentage
+        thresh_vals = np.count_nonzero( (cf_img / 255) > threshold_percentage )
+
+        # Calculate cloud fraction in oktas
+        cf_okta = 8 - int( ( thresh_vals / cf_img.size ) * 8 )
+
+        # Convert cloud fraction to named cloud cover in oktas
+        cf_names = {
+                0 : "CLR",
+               
+                1 : "FEW",
+                2 : "FEW",
+               
+                3 : "SCT",
+                4 : "SCT",
+               
+                5 : "BKN",
+                6 : "BKN",
+                7 : "BKN",
+               
+                8 : "OVC"
+                }
+        
+        # get the appropriate octa name 
+        cf_name = cf_names[cf_okta]
+        
+        print('Cloud Cover for image {}:'.format(count), cf_okta, cf_name)
+        
+        # store data in list
+        cloud_fraction.append(cf_name)
+        cf_oktas.append(cf_okta)
+        
+    
+    print('All images processed!')
+    
+    df['Cloud Fraction'] = cloud_fraction
+    df['Okta'] = cf_oktas
+    
+    savepath = '../swirll-data/'
+    
+    df.to_csv(savepath+'2020_cloud_fraction_swirll.csv')
+    
+    return df
     
     
     
